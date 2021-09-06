514a4d186e69877eda9cca44a5af3debc37f6085
m00k <christian.bican@gmail.com>
2021-09-06 10:44:19 +0200

    fix svg

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent, h } from 'preact';
 import React from 'preact/compat'; // NOTE: required for fragment <>
+import { Logo } from './logo';
 
 const App: FunctionComponent = () => {
   return (
     <>
+    <Logo />
       <p>Hello Vite + Preact!</p>
       <p>
         <a
