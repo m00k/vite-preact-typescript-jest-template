7419398b490290a343040dd9a439f668a0c97149
m00k <christian.bican@gmail.com>
2021-09-09 03:17:15 +0200

    reset to initial config for dev, build; extract jest jsx settings to tsconfig.test.json

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  }
 
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-// import preact from '@preact/preset-vite'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [preact()],
-// })
-
 export default defineConfig({
-  esbuild: {
-    jsxInject: `import React from 'preact/compat'`
-  }
+  plugins: [preact()],
 })
+
+// export default defineConfig({
+//   esbuild: {
+//     jsxInject: `import React from 'preact/compat'`
+//   }
+// })
