6ea3e50e7d7d6ba9f37c24f74d2ed3977e778a48
m00k <christian.bican@gmail.com>
2022-09-17 07:22:01 +0200

    silence esbuild warning this-is-undefined-in-esm

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
 })
 
 // export default defineConfig({
